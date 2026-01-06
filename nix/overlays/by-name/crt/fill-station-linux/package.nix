6718d3309642d3a6d646c87951444c8f88ca455b
Max Larsson <maxslarsson@gmail.com>
2026-01-05 19:44:19 -0500

    Revert "Restrict fill-station-linux to build on linux"
    This reverts commit cc6cc896e9fb0273c1e309b09d3b829284926b77.

--- nix/overlays/by-name/crt/fill-station-linux/package.nix
+++ nix/overlays/by-name/crt/fill-station-linux/package.nix
@@ -1,6 +1,5 @@
-{ lib, linuxKernel, linux_latest }:
+{ linuxKernel, linux_latest }:
 linuxKernel.manualConfig {
   inherit (linux_latest) src version modDirVersion;
   configfile = ./kernel.config;
-  meta.platforms = lib.platforms.linux;
 }
