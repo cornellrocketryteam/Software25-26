9ab17513f0350ad1d7fa239e52046e84929af943
ronit <rt526@cornell.edu>
2026-01-15 16:59:11 -0500

    test ignitor w/o wifi

--- fill-station/src/hardware.rs
+++ fill-station/src/hardware.rs
@@ -18,10 +18,10 @@ const ADC1_ADDRESS: u16 = 0x48;
 const ADC2_ADDRESS: u16 = 0x49;
 
 pub struct Hardware {
-    #[cfg(any(target_os = "linux", target_os = "android"))]
-    pub ig1: Igniter,
-    #[cfg(any(target_os = "linux", target_os = "android"))]
-    pub ig2: Igniter,
+    // #[cfg(any(target_os = "linux", target_os = "android"))]
+    // pub ig1: Igniter,
+    // #[cfg(any(target_os = "linux", target_os = "android"))]
+    // pub ig2: Igniter,
     pub adc1: Ads1015,
     pub adc2: Ads1015,
     #[cfg(any(target_os = "linux", target_os = "android"))]
@@ -43,8 +43,8 @@ impl Hardware {
     pub async fn new() -> Result<Self> {
         let chip0 = Chip::new(GPIO_CHIP0).await?;
         let chip1 = Chip::new(GPIO_CHIP1).await?;
-        let ig1 = Igniter::new(&chip0, 39, &chip0, 38).await?; // 38 is signal, 39 is continuity
-        let ig2 = Igniter::new(&chip1, 42, &chip0, 40).await?; // 42 is continuity on chip 1, 40 is signal on chip 0
+        // let ig1 = Igniter::new(&chip0, 39, &chip0, 38).await?; // 38 is signal, 39 is continuity
+        // let ig2 = Igniter::new(&chip1, 42, &chip0, 40).await?; // 42 is continuity on chip 1, 40 is signal on chip 0
         
         let adc1 = Ads1015::new(I2C_BUS, ADC1_ADDRESS)?;
         let adc2 = Ads1015::new(I2C_BUS, ADC2_ADDRESS)?;
@@ -96,7 +96,7 @@ impl Hardware {
             "BallValve"
         ).await?;
 
-        Ok(Self { ig1, ig2, adc1, adc2, sv1, sv2, sv3, sv4, sv5, mav, ball_valve })
+        Ok(Self { /* ig1, ig2, */ adc1, adc2, sv1, sv2, sv3, sv4, sv5, mav, ball_valve })
     }
 
     #[cfg(not(any(target_os = "linux", target_os = "android")))]
