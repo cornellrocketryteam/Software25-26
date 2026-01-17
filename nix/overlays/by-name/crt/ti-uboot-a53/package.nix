152d4803c25b9d5450ef60706a6cbe92d29d3283
ronit <rt526@cornell.edu>
2026-01-16 20:53:16 -0500

    didnt work - new fix

--- nix/overlays/by-name/crt/ti-uboot-a53/package.nix
+++ nix/overlays/by-name/crt/ti-uboot-a53/package.nix
@@ -13,6 +13,10 @@ buildUBoot {
   defconfig = defconfigName;
   preConfigure = ''
     cp ${defconfigFile} configs/${defconfigName}
+    for f in $(find arch/arm/dts -name "k3-am642-sk.dts" -o -name "k3-am642-r5-sk.dts"); do
+      echo "" >> $f
+      echo "&epwm5 { status = \"disabled\"; };" >> $f
+    done
   '';
   extraPatches = [
     ./patches/ti-board-detect-fixes.patch
