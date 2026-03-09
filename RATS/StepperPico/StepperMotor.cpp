0b8b2b990aaea02aa5c2fa560973974cc5ed0ba2
arazack1210 <amirarazack@gmail.com>
2026-03-08 23:44:51 -0400

    potential fix?

--- RATS/StepperPico/StepperMotor.cpp
+++ RATS/StepperPico/StepperMotor.cpp
@@ -13,8 +13,12 @@ StepperMotor::StepperMotor(uint8_t DIR, uint8_t STEP, uint8_t EN,
   if (EN != 255) {
     motor_.setEnablePin(EN);
     // Default active low enable for most stepper
-    // drivers (e.g. TMC, A4988)
-    motor_.setPinsInverted(false, false, true);
+    // drivers (e.g. TMC, A4988), also step and dir are active low
+    motor_.setPinsInverted(true, true, true);
+
+    // NEMA 23 closed-loop drivers usually have opto-isolated inputs that need a
+    // slightly longer pulse
+    motor_.setMinPulseWidth(10);
   }
 }
 
