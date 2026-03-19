34596366da8874b7a1e3e83d63d7deffa4796c38
ronit <rt526@cornell.edu>
2026-03-19 14:50:36 -0400

    FINAL MAV VALS

--- fsw/src/actuator.rs
+++ fsw/src/actuator.rs
@@ -144,9 +144,9 @@ impl<'a> Mav<'a> {
     const SERVO_PERIOD_US: u16 = (1_000_000 / Self::SERVO_FREQ_HZ) as u16; // 3030 µs
     const SERVO_MIN_US: u16 = 800;
     const SERVO_MAX_US: u16 = 2200;
-    const SERVO_OPEN_US: u16 = 1900;
-    const SERVO_CLOSE_US: u16 = 1350;
-    const SERVO_NEUTRAL_US: u16 = 1520;
+    const SERVO_OPEN_US: u16 = 1950;
+    const SERVO_CLOSE_US: u16 = 883;
+    const SERVO_NEUTRAL_US: u16 = 1300;
 
     /// Create new MAV servo driver.
     /// Assumes PWM slice already configured for:
