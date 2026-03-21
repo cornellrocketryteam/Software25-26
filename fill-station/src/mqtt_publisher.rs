a6601498dc5519eae18854a511837184bdc4b497
ronit <rt526@cornell.edu>
2026-03-21 16:31:56 -0400

    test

--- fill-station/src/mqtt_publisher.rs
+++ fill-station/src/mqtt_publisher.rs
@@ -14,7 +14,7 @@ use crate::command::{AdcReadings, UmbilicalReadings};
 // ============================================================================
 
 /// IP address of the EMQX MQTT broker
-const BROKER_HOST: &str = "192.168.0.101";
+const BROKER_HOST: &str = "192.168.1.206";
 
 /// MQTT broker port
 const BROKER_PORT: u16 = 1883;
