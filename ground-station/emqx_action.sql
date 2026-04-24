068988bd65957da0b3bc7ecc3101a0c4c2322015
Owen Qu <103903453+OQ901@users.noreply.github.com>
2026-04-24 04:15:23 -0400

    Updated timestamping from Rust packet timestamp

--- ground-station/emqx_action.sql
+++ ground-station/emqx_action.sql
@@ -12,7 +12,7 @@ INSERT INTO telemetry_data(
   pt_1_pressure, pt_2_pressure, ball_valve_open, sv_1_open, load_cell, ignition, qd_state
 )
 VALUES (
-  NOW(), ${unit_id}, ${sync_word}, ${flight_mode}, ${pressure}, ${temp}, ${altitude}, 
+  to_timestamp(EXTRACT(EPOCH FROM date_trunc('day', NOW() AT TIME ZONE 'UTC')) + ${timestamp}), ${unit_id}, ${sync_word}, ${flight_mode}, ${pressure}, ${temp}, ${altitude}, 
   ${latitude}, ${longitude}, ${num_satellites}, ${timestamp}, ${mag_x}, ${mag_y}, ${mag_z}, 
   ${accel_x}, ${accel_y}, ${accel_z}, ${gyro_x}, ${gyro_y}, ${gyro_z}, ${pt3}, ${pt4}, ${rtd}, 
   ${sv_2_open}, ${mav_open}, 
