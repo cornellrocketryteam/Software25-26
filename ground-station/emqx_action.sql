0b85da5556aa6bd77fc1e9a8650c4d3099a9cee4
Owen Qu <103903453+OQ901@users.noreply.github.com>
2026-04-26 02:12:45 -0400

    Insert command Action.SQL

--- ground-station/emqx_action.sql
+++ ground-station/emqx_action.sql
@@ -1,26 +1,177 @@
-INSERT INTO telemetry_data(
-  time, unit_id, sync_word, flight_mode, pressure, temp, altitude, 
-  latitude, longitude, num_satellites, timestamp, mag_x, mag_y, mag_z, 
-  accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z, pt3, pt4, rtd, 
-  sv_2_open, mav_open, ms_since_boot_cfc, ms_since_boot_fill,
-  ssa_drogue_deployed, ssa_main_deployed, cmd_n1, cmd_n2, cmd_n3, cmd_n4, cmd_a1, cmd_a2, cmd_a3,
-  airbrake_state, predicted_apogee,
-  h_acc, v_acc, vel_n, vel_e, vel_d, g_speed, s_acc, head_acc, fix_type, head_mot,
-  blims_motor_position, blims_phase_id, blims_pid_p, blims_pid_i, blims_bearing, blims_loiter_step,
-  blims_heading_des, blims_heading_error, blims_error_integral, blims_dist_to_target_m,
-  blims_target_lat, blims_target_lon, blims_wind_from_deg,
-  pt_1_pressure, pt_2_pressure, ball_valve_open, sv_1_open, load_cell, ignition, qd_state
-)
-VALUES (
-  to_timestamp(EXTRACT(EPOCH FROM date_trunc('day', NOW() AT TIME ZONE 'UTC')) + ${timestamp}), ${unit_id}, ${sync_word}, ${flight_mode}, ${pressure}, ${temp}, ${altitude}, 
-  ${latitude}, ${longitude}, ${num_satellites}, ${timestamp}, ${mag_x}, ${mag_y}, ${mag_z}, 
-  ${accel_x}, ${accel_y}, ${accel_z}, ${gyro_x}, ${gyro_y}, ${gyro_z}, ${pt3}, ${pt4}, ${rtd}, 
-  ${sv_2_open}, ${mav_open}, ${ms_since_boot_cfc}, ${ms_since_boot_fill},
-  ${ssa_drogue_deployed}, ${ssa_main_deployed}, ${cmd_n1}, ${cmd_n2}, ${cmd_n3}, ${cmd_n4}, ${cmd_a1}, ${cmd_a2}, ${cmd_a3},
-  ${airbrake_state}, ${predicted_apogee},
-  ${h_acc}, ${v_acc}, ${vel_n}, ${vel_e}, ${vel_d}, ${g_speed}, ${s_acc}, ${head_acc}, ${fix_type}, ${head_mot},
-  ${blims_motor_position}, ${blims_phase_id}, ${blims_pid_p}, ${blims_pid_i}, ${blims_bearing}, ${blims_loiter_step},
-  ${blims_heading_des}, ${blims_heading_error}, ${blims_error_integral}, ${blims_dist_to_target_m},
-  ${blims_target_lat}, ${blims_target_lon}, ${blims_wind_from_deg},
-  ${pt_1_pressure}, ${pt_2_pressure}, ${ball_valve_open}, ${sv_1_open}, ${load_cell}, ${ignition}, ${qd_state}
-)
+INSERT INTO telemetry_data (
+    -- The Primary Live X-Axis Clock
+    time,
+    
+    -- Unit ID
+    unit_id,
+
+    -- Top-Level Packet Info
+    sync_word,
+
+    -- Shared Telemetry (Rust 'Packet' Struct)
+    flight_mode,
+    pressure,
+    temp,
+    altitude,
+    latitude,
+    longitude,
+    num_satellites,
+    gps_time, 
+    mag_x,
+    mag_y,
+    mag_z,
+    accel_x,
+    accel_y,
+    accel_z,
+    gyro_x,
+    gyro_y,
+    gyro_z,
+    pt3,
+    pt4,
+    rtd,
+    sv_2_open,
+    mav_open,
+    ms_since_boot_cfc,
+
+    -- Event Flags
+    ssa_drogue_deployed,
+    ssa_main_deployed,
+    cmd_n1,
+    cmd_n2,
+    cmd_n3,
+    cmd_n4,
+    cmd_a1,
+    cmd_a2,
+    cmd_a3,
+
+    -- Airbrake & Control States
+    airbrake_state,
+    predicted_apogee,
+
+    -- Advanced GPS / U-Blox Metrics
+    h_acc,
+    v_acc,
+    vel_n,
+    vel_e,
+    vel_d,
+    g_speed,
+    s_acc,
+    head_acc,
+    fix_type,
+    head_mot,
+
+    -- BLiMS Outputs
+    blims_motor_position,
+    blims_phase_id,
+    blims_pid_p,
+    blims_pid_i,
+    blims_bearing,
+    blims_loiter_step,
+    blims_heading_des,
+    blims_heading_error,
+    blims_error_integral,
+    blims_dist_to_target_m,
+
+    -- BLiMS Config
+    blims_target_lat,
+    blims_target_lon,
+    blims_wind_from_deg,
+
+    -- Fill Station Specific (Umbilical)
+    pt_1_pressure,
+    pt_2_pressure,
+    ball_valve_open,
+    sv_1_open,
+    load_cell,
+    ignition,
+    qd_state,
+    ms_since_boot_fill
+) VALUES (
+    -- Convert EMQX's internal arrival clock into a standard PostgreSQL TIMESTAMPTZ
+    to_timestamp(${broker_arrival_ms} / 1000.0),
+    
+    -- Unit ID
+    ${unit_id},
+
+    -- Top-Level Packet Info
+    ${sync_word},
+
+    -- Shared Telemetry
+    ${flight_mode},
+    ${pressure},
+    ${temp},
+    ${altitude},
+    ${latitude},
+    ${longitude},
+    ${num_satellites},
+    ${gps_time},
+    ${mag_x},
+    ${mag_y},
+    ${mag_z},
+    ${accel_x},
+    ${accel_y},
+    ${accel_z},
+    ${gyro_x},
+    ${gyro_y},
+    ${gyro_z},
+    ${pt3},
+    ${pt4},
+    ${rtd},
+    ${sv_2_open},
+    ${mav_open},
+    ${ms_since_boot_cfc},
+
+    -- Event Flags
+    ${ssa_drogue_deployed},
+    ${ssa_main_deployed},
+    ${cmd_n1},
+    ${cmd_n2},
+    ${cmd_n3},
+    ${cmd_n4},
+    ${cmd_a1},
+    ${cmd_a2},
+    ${cmd_a3},
+
+    -- Airbrake & Control States
+    ${airbrake_state},
+    ${predicted_apogee},
+
+    -- Advanced GPS / U-Blox Metrics
+    ${h_acc},
+    ${v_acc},
+    ${vel_n},
+    ${vel_e},
+    ${vel_d},
+    ${g_speed},
+    ${s_acc},
+    ${head_acc},
+    ${fix_type},
+    ${head_mot},
+
+    -- BLiMS Outputs
+    ${blims_motor_position},
+    ${blims_phase_id},
+    ${blims_pid_p},
+    ${blims_pid_i},
+    ${blims_bearing},
+    ${blims_loiter_step},
+    ${blims_heading_des},
+    ${blims_heading_error},
+    ${blims_error_integral},
+    ${blims_dist_to_target_m},
+
+    -- BLiMS Config
+    ${blims_target_lat},
+    ${blims_target_lon},
+    ${blims_wind_from_deg},
+
+    -- Fill Station Specific
+    ${pt_1_pressure},
+    ${pt_2_pressure},
+    ${ball_valve_open},
+    ${sv_1_open},
+    ${load_cell},
+    ${ignition},
+    ${qd_state},
+    ${ms_since_boot_fill}
+);
